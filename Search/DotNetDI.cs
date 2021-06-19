34a2b6ef7ff9c8b13cc63b08614ae165b13c8e80
Kyle Lauffer <kcl5072@gmail.com>
2021-06-19 08:48:51 -0400

    added comments to the DI Helper

--- Search/DotNetDI.cs
+++ Search/DotNetDI.cs
@@ -4,8 +4,16 @@ using Search.FuzzySearch;
 
 namespace Search
 {
+    /// <summary>
+    /// helper class to provide an easy way to setup the Search inside of the DotNet DI system
+    /// </summary>
     public static class DotNetDI
     {
+        /// <summary>
+        /// Sets up the DI mappings for the DotNet DI system
+        /// </summary>
+        /// <param name="service">the DotNet Di ServiceCollection</param>
+        /// <param name="fuzzySearchType">the type of search algorithm to be run</param>
         public static void AddSearch(this IServiceCollection service, FuzzySearchType fuzzySearchType)
         {
             service.AddTransient(provider => 
