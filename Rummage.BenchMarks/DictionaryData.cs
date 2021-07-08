3f0a875c36afe594fdb8aa5e03298bc2fd6391fe
Kyle Lauffer <kcl5072@gmail.com>
2021-07-08 19:05:42 -0400

    Added proper bench marking

---
+++ Rummage.BenchMarks/DictionaryData.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace Rummage.BenchMarks
+{
+    public static class DictionaryData
+    {
+        public static HashSet<IndexItem<int>> GetData(string filePath)
+        {
+            return new HashSet<IndexItem<int>>(ReadFile(filePath));
+        }
+
+        private static IEnumerable<IndexItem<int>> ReadFile(string file)
+        {
+            string line;
+            var id = 1;
+            using var reader = File.OpenText(file);
+            while ((line = reader.ReadLine()) != null)
+            {
+                var newRecord = IndexItem<int>.From(PhraseId<int>.From(id), line);
+                yield return newRecord;
+                id++;
+            }
+        }
+    }
+}
---
+++ Rummage.BenchMarks/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace Rummage.BenchMarks
+{
+    public class SearchBenchmarks
+    {
+        private ILogger Logger => Mock.Of<ILogger>();
+        private readonly HashSet<IndexItem<int>> ExternalData = DictionaryData.GetData(@"Dictionary.txt");
+
+        [Benchmark]
+        public async Task<List<SearchResult<int>>> LevenshteinSearch()
+        {
+            var searchEngine = new Rummage.SearchEngine<int>(FuzzySearch.FuzzySearchType.Levenshtein, Logger);
+            var result = await searchEngine.Search("idempotent", ExternalData);
+            return result.ToList();
+        }
+
+        [Benchmark]
+        public async Task<List<SearchResult<int>>> DamerauLevenshteinSearch()
+        {
+            var searchEngine = new Rummage.SearchEngine<int>(FuzzySearch.FuzzySearchType.DamerauLevenshtein, Logger);
+            var result = await searchEngine.Search("idempotent", ExternalData);
+            return result.ToList();
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<SearchBenchmarks>();
+        }
+    }
+}
